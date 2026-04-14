22383111a8f7959c66387154ef69530cb5035066
ildarmuhamet250784-a11y <ildarmuhamet250784@gmail.com>
2026-04-14 06:38:52 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        var age=20;
+        boolean adult=age>=18;
+        if (adult){
+            System.out.println("Если возраст человека равен 18 или больше, то он совершеннолетний");
+        }else {
+            System.out.println("Если возраст человека меньше 18, он не достиг совершеннолетия и нужно немного подождать");
 
+        }
 
 
     }
